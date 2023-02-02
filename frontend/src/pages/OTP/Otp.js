69ee35e65d641f7e4420eeb9c915c24a910c5bb0
Dhritiman-Das <iamdhritiman01@gmail.com>
2023-02-02 11:31:38 +0530

    Resolved the Otp.js conflict

--- frontend/src/pages/OTP/Otp.js
+++ frontend/src/pages/OTP/Otp.js
@@ -8,13 +8,45 @@ import { otpActions } from "../../store/otpSlice";
 const Otp = () => {
   const data = useSelector((state) => state.otp);
   console.log(data);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [seconds, setSeconds] = useState(5);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      dispatch(otpActions.timeDecay());
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
 
+      if (seconds === 0) {
+        return clearInterval(interval);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [seconds]);
+  useEffect(() => {
+    if (data.authorized) {
+      console.log("Should Route to a new page now");
+      navigate("/setup");
+    }
+  }, [data.authorized]);
   const otpboxes = useRef("");
   const removeClassname = (elem) => {
     Array.from(elem).forEach((div) => {
       div.classList.remove("activeInput");
     });
   };
+  const finalOtp = (elem) => {
+    let finalotp = "";
+    Array.from(elem).forEach((div) => {
+      finalotp = finalotp + div.value;
+    });
+    return finalotp;
+  };
+
   const clickEve = (event, next) => {
     let curr = event.target;
     if (curr.value.length) {
@@ -25,17 +57,17 @@ const Otp = () => {
       otpboxes.current.children[next - 1].disabled = true;
     }
   };
-};
-const backward = (event, prev) => {
-  let curr = event.target;
-  if (!curr.value.length && event.keyCode == "8") {
-    otpboxes.current.children[prev].disabled = false;
-    removeClassname(otpboxes.current.children);
-    otpboxes.current.children[prev].classList.add("activeInput");
-    otpboxes.current.children[prev].focus();
-    otpboxes.current.children[prev].value = "";
-    otpboxes.current.children[prev + 1].disabled = true;
-  }
+  const backward = (event, prev) => {
+    let curr = event.target;
+    if (!curr.value.length && event.keyCode == "8") {
+      otpboxes.current.children[prev].disabled = false;
+      removeClassname(otpboxes.current.children);
+      otpboxes.current.children[prev].classList.add("activeInput");
+      otpboxes.current.children[prev].focus();
+      otpboxes.current.children[prev].value = "";
+      otpboxes.current.children[prev + 1].disabled = true;
+    }
+  };
   return (
     <div className="bg-[#1A1C20] h-screen flex items-center">
       <div className="bg-[#000000] w-[700px] h-[400px] rounded-lg drop-shadow-xl m-auto text-white-100 p-6">
@@ -59,7 +91,7 @@ const backward = (event, prev) => {
             <input
               type="text"
               autoFocus
-              max={1}
+              maxLength={1}
               className="bg-[#000000] w-16 h-17 p-4 border-2 border-gray-600 ml-6"
               onKeyUp={(e) => {
                 clickEve(e, 1);
@@ -123,13 +155,29 @@ const backward = (event, prev) => {
               }}
             />
           </div>
-          <div className="resend text-[16px] font-medium text-[#5D5FEF] mt-6 text-right cursor-pointer">
-            Resend code in 15 secs...
+          <div className="resend text-[16px] font-medium text-[#5D5FEF] mt-6 text-right">
+            {seconds > 0 ? (
+              <div>Resend code in {seconds} secs...</div>
+            ) : (
+              <div
+                onClick={() => {
+                  setSeconds(5);
+                  dispatch(otpActions.timeReset());
+                }}
+                className="cursor-pointer hover:underline"
+              >
+                Resend code
+              </div>
+            )}
           </div>
           <div className="btn pt-6 text-center">
             <button
               className="w-64 bg-white-100 hover:bg-gray-400 text-[#000000] font-medium text-xl pb-1 rounded-2xl"
-              onClick={clickEve}
+              onClick={(e) =>
+                dispatch(
+                  otpActions.otpAdded(finalOtp(otpboxes.current.children))
+                )
+              }
             >
               Confirm
             </button>
