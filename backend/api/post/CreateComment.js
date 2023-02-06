5dd874a9a41b31621761fef88eb1f8b84d670c57
Dhritiman-Das <iamdhritiman01@gmail.com>
2023-02-06 15:06:56 +0530

    Post routes done

--- backend/api/post/CreateComment.js
+++ backend/api/post/CreateComment.js
@@ -19,101 +19,73 @@ export const createComment = asyncHandler(async (req, res) => {
       stats,
       comments
     );
-    const isParentPostEmpty = false;
+    const isParentPostIdEmpty = false;
     const isCreatorEmpty = false;
     const isContentEmpty = false;
-    if (isParentPostEmpty || isCreatorEmpty || isContentEmpty) {
+    if (isParentPostIdEmpty || isCreatorEmpty || isContentEmpty) {
       throw new Error("Empty entries");
     } else {
       // Check if userId is valid
-      await User.findById(mongoose.Types.ObjectId(creator.id))
-        .then((user) => {
-          console.log("User is ", user);
-          if (!user) {
-            console.log("Entered here");
-            throw new Error("User not found.");
-          }
-          //user present
-          // Check if parentCommentId exists
-          if (parentCommentId) {
-            const parentComment = Comment.findById(parentCommentId)
-              .then((comment) => {
-                if (!comment) {
-                  //parentComment is not valid
-                  console.log("Comment is ", comment);
-                  throw new Error("Parent comment not found.");
-                }
-              })
+      const user = await User.findById(mongoose.Types.ObjectId(creator.id));
+      if (!user) {
+        throw new Error("User not found.");
+      }
 
-              .catch((error) => {
-                res.json({
-                  status: "FAILED",
-                  message: error.message,
-                });
-              });
-          }
-          //parentId is valid or empty
-          const newComment = new Comment({
-            parentPostId,
-            parentCommentId,
-            creator,
-            content,
-            stats,
-            comments,
-          });
-          newComment
-            .save()
-            .then((newComment) => {
-              console.log("Comment saved ", newComment);
-              if (!parentCommentId) {
-                // It is a parent comment, update the post
-                const post = Post.findOneAndUpdate(
-                  { _id: parentPostId },
-                  {
-                    $inc: { "comments.count": 1 },
-                    $push: { "comments.commentsList": newComment._id },
-                  },
-                  { new: true }
-                )
-                  .then((updatedPost) => {
-                    res.json({
-                      status: "SUCCESS",
-                      message: "Comment Added successfully",
-                    });
-                  })
-                  .catch();
-              } else {
-                // It is a child comment, update the parent comment
-                const updatedComment = Comment.findOneAndUpdate(
-                  { _id: parentCommentId },
-                  {
-                    $inc: { "comments.count": 1 },
-                    $push: { "comments.commentsList": newComment._id },
-                  },
-                  { new: true }
-                )
-                  .then((updatedComment) => {
-                    res.json({
-                      status: "SUCCESS",
-                      message: "Comment Added successfully",
-                    });
-                  })
-                  .catch();
-              }
-            })
-            .catch();
-        })
-        .catch((error) => {
-          res.json({
-            status: "FAILED",
-            message: "2" + error.message,
-          });
-        });
+      // Check if parentCommentId exists
+      if (parentCommentId) {
+        const parentComment = await Comment.findById(parentCommentId);
+        if (!parentComment) {
+          throw new Error("Parent comment not found.");
+        }
+      }
+
+      // Check if parentPostId is valid
+      const post = await Post.findById(parentPostId);
+      if (!post) {
+        throw new Error("Parent Post not found.");
+      }
+
+      const newComment = new Comment({
+        parentPostId,
+        parentCommentId,
+        creator,
+        content,
+        stats,
+        comments,
+      });
+      const savedComment = await newComment.save();
+
+      // It is a parent comment, update the post
+      if (!parentCommentId) {
+        await Post.findOneAndUpdate(
+          { _id: parentPostId },
+          {
+            $inc: { "comments.count": 1 },
+            $push: { "comments.commentsList": savedComment._id },
+          },
+          { new: true }
+        );
+      } else {
+        // It is a child comment, update the parent comment
+        await Comment.findOneAndUpdate(
+          { _id: parentCommentId },
+          {
+            $inc: { "comments.count": 1 },
+            $push: { "comments.commentsList": savedComment._id },
+          },
+          { new: true }
+        );
+      }
+
+      res.json({
+        status: "SUCCESS",
+        message: "Comment Added successfully",
+      });
     }
   } catch (error) {
     res.json({
       status: "FAILED",
-      message: "1" + error.message,
+      message: error.message,
     });
   }
 });
--- backend/api/post/DeleteComment.js
+++ backend/api/post/DeleteComment.js
@@ -1,52 +1,85 @@
 import express from "express";
+import mongoose from "mongoose";
 import asyncHandler from "express-async-handler";
+import Comment from "../../models/Comments.js";
 import Post from "../../models/Posts.js";
 
 const router = express.Router();
 
-export const deletePost = asyncHandler(async (req, res) => {
+export const deleteComment = asyncHandler(async (req, res) => {
   try {
     // const userId = req.user._id;
     const userId = req.headers.authorization.split(" ")[1];
-    const postId = req.params.id;
+    const commentId = req.params.id;
     console.log("User ID is ", userId);
-    console.log("Post ID is ", postId);
+    console.log("Comment ID is ", commentId);
 
-    Post.findOne({ _id: postId })
-      .then((post) => {
-        if (!post) {
+    Comment.findById(mongoose.Types.ObjectId(commentId))
+      .then((comment) => {
+        console.log("Deleted comment is ", comment);
+        const parentPostId = comment.parentPostId;
+        const parentCommentId = comment.parentCommentId;
+        if (!comment) {
           return res
             .status(404)
-            .json({ status: "FAILED", message: "Post not found." });
+            .json({ status: "FAILED", message: "Comment not found." });
         }
-
-        if (post.creator.id !== userId) {
+        if (comment.creator.id !== userId) {
           console.log("Request not authorized");
           return res
             .status(401)
             .json({ status: "FAILED", message: "Unauthorized request." });
         } else {
-          post
-            .remove()
-            .then((post) => {
-              return res
-                .status(200)
-                .json({ message: "Post deleted successfully" });
+          Comment.deleteOne({ _id: commentId })
+            .then((comment) => {
+              if (parentCommentId) {
+                //Parent comment Id exists. This means the comment is not a top level comment
+                Comment.updateOne(
+                  { _id: parentCommentId },
+                  {
+                    $pull: { "comments.commentsList": commentId },
+                    $inc: { "comments.count": -1 },
+                  }
+                )
+                  .then((parentComment) => {
+                    console.log("Parent Comment modified", parentComment);
+                    return res
+                      .status(200)
+                      .json({ message: "Comment deleted successfully" });
+                  })
+                  .catch((error) => {
+                    res.json({
+                      status: "FAILED",
+                      message: error.message,
+                    });
+                  });
+              } else {
+                // The comment to be deleted is a top level comment
+                Post.updateOne(
+                  { _id: parentPostId },
+                  {
+                    $pull: { "comments.commentsList": commentId },
+                    $inc: { "comments.count": -1 },
+                  }
+                )
+                  .then((parentPost) => {
+                    console.log("Parent Post modified", parentPost);
+                    return res
+                      .status(200)
+                      .json({ message: "Comment deleted successfully" });
+                  })
+                  .catch((error) => {
+                    res.json({
+                      status: "FAILED",
+                      message: error.message,
+                    });
+                  });
+              }
             })
-            .catch((error) => {
-              res.json({
-                status: "FAILED",
-                message: error.message,
-              });
-            });
+            .catch();
         }
       })
-      .catch((error) => {
-        res.json({
-          status: "FAILED",
-          message: error.message,
-        });
-      });
+      .catch();
   } catch (error) {
     res.json({
       status: "FAILED",
