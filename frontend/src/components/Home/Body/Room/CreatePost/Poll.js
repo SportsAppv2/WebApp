8e63cff364c2fc896bb2ca4b1cc3f5ac5d82d407
Dhritiman-Das <iamdhritiman01@gmail.com>
2023-02-06 15:20:16 +0530

    Displayed the poll icon in create Post

--- frontend/src/components/Home/Body/Room/CreatePost/Poll.js
+++ frontend/src/components/Home/Body/Room/CreatePost/Poll.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import ChoiceArea from './ChoiceArea';
+import React from "react";
+import ChoiceArea from "./ChoiceArea";
 
 const Poll = () => {
-    return (
-        <div className='bg-[#000000] text-white-100 p-2'>
-            <div className=''>
-                <input type="text" 
-                placeholder='Ask a question...' 
-                className='bg-[#000000] text-white-100 w-[90%]'
-                />
-            </div>
-            <ChoiceArea />
-            <ChoiceArea />
-            <div className=''>
-                <button className='bg-[#5D5FEF] bg-opacity-50 hover:bg-blue-100 p-1 rounded-lg my-2 text-white-100 font-medium'>
-                    Create Poll
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="bg-[#000000] text-white-100 p-2 absolute top-[-160px] border-2 border-gray-600 h-[fit] min-w-max text-sm">
+      <div className="">
+        <input
+          type="text"
+          placeholder="Ask a question..."
+          className="bg-[#000000] text-white-100 w-[90%]"
+        />
+      </div>
+      <ChoiceArea />
+      <ChoiceArea />
+      <div className="">
+        <button className="bg-[#5D5FEF] bg-opacity-50 hover:bg-blue-100 p-1 rounded-lg my-2 text-white-100 font-medium">
+          Create Poll
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Poll;
