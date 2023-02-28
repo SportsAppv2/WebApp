296a0cf95820528e6e0790f70619988824970f6c
Baibhav Saikia <baibhavsk@gmail.com>
2023-02-28 21:14:26 +0530

    format updated

--- frontend/src/components/Home/Tournament/SingleParticipant.js
+++ frontend/src/components/Home/Tournament/SingleParticipant.js
@@ -3,11 +3,8 @@ import { useSelector } from 'react-redux';
 
 const SingleParticipant = (props) => {
     const data = useSelector((state) => state.tournament);
-    const handleDragStart = (e) => {
-        e.dataTransfer.setData('text/plain' , props.val.name);
-    };
     return (
-        <div className='bg-[#333030] w-[300px] h-fit text-white-100 font-medium text-[18px] p-3 rounded-lg m-5 cursor-pointer hover:bg-[#232121]' draggable={true} onDragStart={(e) => {handleDragStart(e)}}>
+        <div className='bg-[#333030] w-[300px] h-fit text-white-100 font-medium text-[18px] p-3 rounded-lg m-5 cursor-pointer hover:bg-[#232121]'>
             {props.val.name}
         </div>
     );
